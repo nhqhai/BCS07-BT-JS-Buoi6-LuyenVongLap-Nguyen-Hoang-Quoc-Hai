fbcbd53d2e01ac7fbab18f9624009260f9316659
Nguyễn Hoàng Quốc Hải <112468220+nhqhai@users.noreply.github.com>
2023-05-23 21:12:09 +0700

    done

--- index.js
+++ index.js
@@ -47,16 +47,46 @@ document.querySelector(".btn-success").onclick = tinhGiaiThuaJ;
 
 // Bài Tập 4: Viết chương trình khi click vào button sẽ in ra 10 thẻ div. Div chẵn = đỏ - Div lẻ = xanh
 
+function hienThiDiv() {
+    var hienThi = document.getElementById("hienThiCacDiv");
+    var soDiv = "";
+    for (var i = 1; i <= 10; i++) {
+      if (i % 2 === 0) {
+        soDiv += "<div style='background-color: red;'>Div chẵn</div>";
+      } else {
+        soDiv += "<div style='background-color: blue;'>Div lẻ</div>";
+      }
+    }
+    document.getElementById ("ketQuaDiv").innerHTML = soDiv;
+  }
+
+  document.querySelector(".btn-primary").onclick = hienThiDiv;
+
 // ---------------------------------------
 
 // BàiTập 5: Viết chương trình có 1 ô input, 1 button. Khi click vào button thì in ra các số nguyên tố từ 1 tới giá trị của ô input
 
-function inSoNguyenTo(){
-    var soH = document.getElementById("soNguyenTo").value *1;
+function kiemTraSoNguyenTo(n) {
+    if (n <= 1) {
+      return false;
+    }
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function inSoNguyenTo() {
+    var soH = document.getElementById("soNguyenTo").value * 1;
     var input = "";
     for (var i = 1; i <= soH; i++) {
-
+      if (kiemTraSoNguyenTo(i)) {
+        input += i + ", ";
+      }
     }
-    document.getElementById("hienThiKetQuaSoNguyenTo").innerHTML = "Các số nguyên tố từ 1 tới " + soNguyenTo + " là:";
-}
-document.querySelector(".btn-danger").onclick=inSoNguyenTo;
+    document.getElementById("hienThiKetQuaSoNguyenTo").innerHTML =
+      "Các số nguyên tố từ 1 tới " + soH + " là: " + input;
+  }
+
+  document.querySelector(".btn-danger").onclick = inSoNguyenTo;
